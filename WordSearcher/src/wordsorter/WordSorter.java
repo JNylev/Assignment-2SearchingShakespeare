adbbdb9e6d41ab45e008f77e8fecda2842d47444
rezalt <ollobrains@gmail.com>
2017-02-28 17:17:30 +0100

    Uploading sorter

--- WordSearcher/src/wordsorter/WordSorter.java
+++ WordSearcher/src/wordsorter/WordSorter.java
@@ -5,8 +5,6 @@
  */
 package wordsorter;
 
-import java.util.Arrays;
-import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -16,107 +14,15 @@ import java.util.concurrent.TimeUnit;
 public class WordSorter
 {
 
-    static String[] wordArray = null;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args)
+    public WordSorter()
     {
-        WordSorter ws = new WordSorter();
-        LoadTextFile textLoader = new LoadTextFile();
-
-        // Here we load up our text of Shakespeare's complete works.
-        wordArray = textLoader.loadText();
-
-        Scanner sc = new Scanner(System.in);
-        int choice;
-        String retry = "y";
-        while (retry.toLowerCase().equals("y") || retry.toLowerCase().equals("yes"))
-        {
-            choice = 0;
-
-            System.out.println("Hello, please insert a number to choose an algorithm for sorting the complete works of Shakespeare."
-                    + "\n\nAvailable options are: "
-                    + "\n1: Merge sort.");
-
-            while (choice < 1 || choice > 1)
-            {
-                try
-                {
-
-                    choice = sc.nextInt();
-
-                    if (choice < 1)
-                    {
-                        System.out.println("The number you have entered is too low."
-                                + "\nPlease try again.");
-                    }
-                    else if (choice > 1)
-                    {
-                        System.out.println("The number you have entered is too high."
-                                + "\nPlease try again.");
-                    }
-
-                }
-                catch (java.util.InputMismatchException e)
-                {
-                    System.out.println("Sorry, you have chosen a wrong input."
-                            + "\nPlease try again.");
-                    sc.nextLine();
-                }
-            }
-
-            ws.sortMethods(choice);
-            System.out.println("\nDo you want to try another sorting method? (y/n)");
-            retry = sc.next();
-        }
-
+      
     }
-
-    public void sortMethods(int choice)
-    {
-        WordSorter ws = new WordSorter();
-        long start;
-        long end;
-        long elapsedTime;
-
-        switch (choice)
-        {
-            case 1:
-
-                // Let's time this
-                start = System.nanoTime();
-                // System.out.println("Start time: " + start);
-
-                // Not sorted!
-                System.out.println("Unsorted: " + Arrays.toString(wordArray.clone()));
-                
-                // Merge sort
-                System.out.println("Beginning to sort text...");
-                // System.out.println("Merge sort: " + Arrays.toString(ws.mergeSort(wordArray.clone())));
-             
-                ws.mergeSort(wordArray.clone());
-                end = System.nanoTime();
-                elapsedTime = end - start;
-                System.out.println("This sorting method took approximately: "
-                        + TimeUnit.NANOSECONDS.toSeconds(elapsedTime) + " seconds"
-                        + " / "
-                        + TimeUnit.NANOSECONDS.toMillis(elapsedTime) + " milliseconds"
-                        + " / "
-                        + elapsedTime + " nanoseconds");
-
-                break;
-
-            default:
-                break;
-        }
-
-    }
-
 
     public String[] mergeSort(String[] words)
     {
+
         if (words.length != 1)
         {
             String[] left = new String[words.length / 2];

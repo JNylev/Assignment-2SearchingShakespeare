dfa00739a4f2a9a39255a8bfc71427a68bda371a
madgolith <jakob.n.joergensen@gmail.com>
2017-02-28 15:53:21 +0100

    del1

--- WordSearcher/src/datastructures/LinkedSymbolTable.java
+++ WordSearcher/src/datastructures/LinkedSymbolTable.java
@@ -8,57 +8,56 @@ package datastructures;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import interfaces.SymbolTable;
+
 
 /**
  *
  * @author jaw40
  */
-public class LinkedSymbolTable<String, Integer>
-{
-
+public class LinkedSymbolTable implements SymbolTable<String, Integer>{
     List<Word> words;
     int size;
-
-    public LinkedSymbolTable()
-    {
+    
+    public LinkedSymbolTable(){
         words = new LinkedList();
     }
-
-    public void put(String key, Integer value)
-    {
+    
+    @Override
+    public void put(String key, Integer value) {
         Word tmp = new Word(key, value);
         int index = words.indexOf(tmp);
-
-        if (index < 0) {
+        
+        if (index < 0){
             words.add(tmp);
             size++;
-
+            
         }
-
-        else {
+        
+        else{
             Word w = words.get(index);
             int orig = w.getCount();
             w.setCount(orig + value);
         }
-
+        
     }
 
-    public Integer get(String key)
-    {
+    @Override
+    public Integer get(String key) {
         int index = words.indexOf(new Word(key, 0));
         return words.get(index).getCount();
-
+        
     }
 
-    public int size()
-    {
+    @Override
+    public int size() {
         return size;
     }
 
-    public Iterable<String> keys()
-    {
+    @Override
+    public Iterable<String> keys() {
         ArrayList<String> al = new ArrayList();
-        for (Word word : words) {
+        for (Word word: words) {
             al.add(word.getWord());
         }
         return al;
